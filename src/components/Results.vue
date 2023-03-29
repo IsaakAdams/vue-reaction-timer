00478dc2c2dddb3ab07d5d65437f78a709d3e3a0
Isaak Adams <125624071+IsaakAdams@users.noreply.github.com>
2023-03-29 00:14:43 -0400

    Initial Commit
    First commit of the vue.js reaction timer project.

---
+++ src/components/Results.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="results-block">
+        <h1>Results:</h1>
+        <p>Reaction time: {{ score }} ms</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['score']
+}
+</script>
+
+<style>
+    .results-block {
+        width: 400px;
+        border-radius: 20px;
+        background-color: crimson;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
